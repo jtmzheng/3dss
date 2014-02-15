30295a39e14be3bc415e162ac167e0068a7412d6
Max Zheng <jtzmax@hotmail.com>
2014-02-15 18:55:06 -0500

    - Style change to Light

--- src/renderer/light/Light.java
+++ src/renderer/light/Light.java
@@ -14,16 +14,16 @@ import org.lwjgl.util.vector.Vector3f;
  */
 public class Light {
 	
-	private Vector3f m_position; // position of camera
-	private Vector3f m_Ls; // white specular colour
-	private Vector3f m_Ld; // dull white diffuse light colour
+	private Vector3f mPosition; // position of camera
+	private Vector3f mLs; // white specular colour
+	private Vector3f mLd; // dull white diffuse light colour
 	@SuppressWarnings("unused")
-	private Vector3f m_La; // grey ambient colour (currently ambient light is global)
+	private Vector3f mLa; // grey ambient colour (currently ambient light is global)
 	
-	private Vector3f m_spot = null; // spot light direction
-	private boolean m_directional;
+	private Vector3f mSpot = null; // spot light direction
+	private boolean mDirectional;
 	
-	private Vector3f m_attenuation;	// Attenuation
+	private Vector3f mAttenuation;	// Attenuation
 	
 	private FloatBuffer m_DataBuffer = BufferUtils.createFloatBuffer(3);
 	
@@ -36,42 +36,42 @@ public class Light {
 	 * @param dir
 	 */
 	public Light(Vector3f pos, Vector3f spec, Vector3f diff, Vector3f ambi, Vector3f dir){
-		m_position = pos;
-		m_Ls = spec;
-		m_Ld = diff;
-		m_La = ambi;
-		m_spot = dir; 
+		mPosition = pos;
+		mLs = spec;
+		mLd = diff;
+		mLa = ambi;
+		mSpot = dir; 
 		
-		m_directional = m_spot != null; // If m_spot is null, it is non-directional
+		mDirectional = mSpot != null; // If m_spot is null, it is non-directional
 		
 		// Default attenuation
-		m_attenuation = new Vector3f(0.3f, 0.007f, 0.008f); //constant, linear, and quadratic
+		mAttenuation = new Vector3f(0.3f, 0.007f, 0.008f); //constant, linear, and quadratic
 	}
 	
 	public void updatePosition(LightGL lgl){
-		m_position.store(m_DataBuffer); m_DataBuffer.flip();
+		mPosition.store(m_DataBuffer); m_DataBuffer.flip();
 		GL20.glUniform3(lgl.getPosition(), m_DataBuffer);
 	}
 	
 	public void updateDirection(LightGL lgl){
-		if(m_directional && m_spot != null) {
-			m_spot.store(m_DataBuffer); m_DataBuffer.flip();
+		if(mDirectional && mSpot != null) {
+			mSpot.store(m_DataBuffer); m_DataBuffer.flip();
 			GL20.glUniform3(lgl.getDirection(), m_DataBuffer);
 		}
 	}
 
 	public void updateSpecular(LightGL lgl){
-		m_Ls.store(m_DataBuffer); m_DataBuffer.flip();
+		mLs.store(m_DataBuffer); m_DataBuffer.flip();
 		GL20.glUniform3(lgl.getSpecular(), m_DataBuffer);
 	}
 	
 	public void updateDiffuse(LightGL lgl){
-		m_Ld.store(m_DataBuffer); m_DataBuffer.flip();
+		mLd.store(m_DataBuffer); m_DataBuffer.flip();
 		GL20.glUniform3(lgl.getDiffuse(), m_DataBuffer);
 	}
 	
 	public void updateAttenuation(LightGL lgl){
-		m_attenuation.store(m_DataBuffer); m_DataBuffer.flip();
+		mAttenuation.store(m_DataBuffer); m_DataBuffer.flip();
 		GL20.glUniform3(lgl.getAttenuation(), m_DataBuffer);
 	}
 	
@@ -82,26 +82,24 @@ public class Light {
 	public void updateIsUsed(LightGL lgl, boolean isUsed){
 		if(isUsed) {
 			GL20.glUniform1f(lgl.getIsUsed(), 1.0f);
-		}
-		else { 
+		} else { 
 			GL20.glUniform1f(lgl.getIsUsed(), 0.0f);
 		}
 	}
 	
 	public void updateIsDirectional(LightGL lgl) {
-		if(m_directional) {
+		if(mDirectional) {
 			GL20.glUniform1f(lgl.getIsDirectional(), 1.0f);
-		}
-		else {
+		} else {
 			GL20.glUniform1f(lgl.getIsDirectional(), 0.0f);
 		}
 	}
 	
 	public void setPosition(Vector3f position) {
-		m_position = position;
+		mPosition = position;
 	}
 
 	public void setDirection(Vector3f direction) {
-		m_spot = direction;
+		mSpot = direction;
 	}
 }
