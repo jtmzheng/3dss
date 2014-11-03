f13ecaf7994e2455de2125b93d6bf88ba44c068a
jtmzheng <jtmzheng@gmail.com>
2014-11-02 19:47:45 -0500

    Remove unused imports

--- src/system/Settings.java
+++ src/system/Settings.java
@@ -2,10 +2,6 @@ package system;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.prefs.Preferences;
-
 import org.ini4j.InvalidFileFormatException;
 import org.ini4j.Wini;
 
